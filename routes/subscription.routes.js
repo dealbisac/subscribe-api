50793e0fa8db8f345be25f58e1ddc52122d518be
dealbisac <chand.dipendra19@gmail.com>
2025-02-24 22:59:18 +0100

    chore: create subscriptions routes

---
+++ routes/subscription.routes.js
@@ -0,0 +1,44 @@
+import { Router } from "express";
+
+const subscriptionRouter = Router();
+
+// Get all subscription
+subscriptionRouter.get('/', (req, res) => {
+    res.send({ title: 'GET all subscriptions'});
+})
+
+// Get subscription by ID
+subscriptionRouter.get('/:id', (req, res) => {
+    res.send({ title: 'GET subscription details'});
+})
+
+// Create new subscription
+subscriptionRouter.post('/', (req, res) => {
+    res.send({ title: 'CREATE new subscription'});
+})
+
+// Update subscription
+subscriptionRouter.put('/:id', (req, res) => {
+    res.send({ title: 'UPDATE subscription'});
+})
+
+// Delete subscription
+subscriptionRouter.delete('/:id', (req, res) => {
+    res.send({ title: 'DELETE subscription.'});
+})
+
+// Get all user subscription
+subscriptionRouter.get('/user/:id', (req, res) => {
+    res.send({ title: 'GET all user subscriptions'});
+})
+
+// Cancel subscription
+subscriptionRouter.put('/:id/cancel', (req, res) => {
+    res.send({ title: 'CANCEL subscriptions'});
+})
+
+subscriptionRouter.get('/upcoming-renewals', (req, res) => {
+    res.send({ title: 'GET upcoming subscriptions'});
+})
+
+export default subscriptionRouter; 
