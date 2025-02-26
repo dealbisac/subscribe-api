eb2eefeee52479fff9dfd55262e5e26d9cb12f37
dealbisac <chand.dipendra19@gmail.com>
2025-02-26 19:07:58 +0100

    create the subscription schema

---
+++ models/subscription.model.js
@@ -0,0 +1,70 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Subscription name is required'],
+        trim: true,
+        minLegth: 2,
+        maxLegth: 100
+    },
+    price: {
+        type: Number,
+        required: [true, 'Subscription price is required'],
+        min: [0, 'Price must be greater than 0'],
+    },
+    currency: {
+        type: String,
+        enum: ['USD', 'EUR', 'GBP'],
+        default: 'USD',
+    },
+    frequency: {
+        type: String,
+        enum: ['daily', 'weekly', 'monthly', 'yearly'],
+    },
+    category: {
+        type: String,
+        enum: ['sports', 'news', 'entertainment', 'lifestyle', 'technology', 'finance', 'politics', 'other'],
+        required: true,
+    },
+    paymentMethod: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    status: {
+        type: String,
+        enum: ['active', 'cancelled', 'expired'],
+        default: 'active',
+    },
+    startDate: {
+        type: Date,
+        required: true,
+        validate: {
+            validator: function (value) {
+                return value <= new Date();
+            },
+            message: 'Start date must be in the past'
+        }
+    },
+    renewalDate: {
+        type: Date,
+        required: true,
+        validate: {
+            validator: function (value) {
+                return value > new Date ();
+            },
+            message: 'Renewal date must be after the start date.'
+        }
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        index: true
+    }
+}, {timestamps: true});
+
+const Subscription = mongoose.model('Subscription', subscriptionSchema);
+
+export default Subscription;
