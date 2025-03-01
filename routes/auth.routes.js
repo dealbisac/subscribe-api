4a6295b504255e49e23e69bbce070de3528d5c38
dealbisac <chand.dipendra19@gmail.com>
2025-03-01 23:37:24 +0100

    add: import the controller in route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,20 +1,15 @@
 import { Router } from 'express';
+import { signIn, signOut, signUp } from '../controllers/auth.controller';
 
 const authRouter = Router();
 
 //Sign up Route
-authRouter.post('/sign-up', (req, res) => {
-    res.send({title: 'Sign up'})
-})
+authRouter.post('/sign-up', signUp);
 
 //Sign in Route
-authRouter.post('/sign-in', (req, res) => {
-    res.send({title: 'Sign in'})
-})
+authRouter.post('/sign-in', signIn);
 
 //Sign out Route
-authRouter.post('/sign-out', (req, res) => {
-    res.send({title: 'Sign out'})
-})
+authRouter.post('/sign-out', signOut)
 
 export default authRouter;
