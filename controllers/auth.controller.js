db876f63d2aa1867fd947b9c9c6c3158d6663e34
dealbisac <chand.dipendra19@gmail.com>
2025-03-01 23:34:52 +0100

    add auth controller logic

---
+++ controllers/auth.controller.js
@@ -0,0 +1,8 @@
+// Signup Logic
+export const signUp = async (req, res, next) => {}
+
+// SignIn Logic
+export const signIn = async (req, res, next) => {}
+
+// SignOut Logic
+export const signOut = async (req, res, next) => {}
