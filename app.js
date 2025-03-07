0c3a67e66688b129124d89e4d01186b3e0bf39ac
dealbisac <chand.dipendra19@gmail.com>
2025-03-07 23:47:54 +0100

    import and use the arcjetmiddleware

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ import subscriptionRouter from './routes/subscription.routes.js';
 
 import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
+import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
 
 
 //  1. Initialize the app
@@ -18,6 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(arcjetMiddleware);
 
 // Use the routes.
 app.use('/api/v1/users', userRouter);
