5ee9e2c68b163a4d61935344e7501bd6d7cb6ce1
dealbisac <chand.dipendra19@gmail.com>
2025-03-10 20:55:12 +0100

    add: create the controller for usercreation

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -26,3 +26,13 @@ export const getUser = async(req, res, next) => {
         next(error);
     }
 }
+
+export const createUser = async (req, res, next) => {
+    try {
+        const user = await User.create({ ...req.body });
+
+        res.status(200).json({ success: true, data: user})
+    } catch (error) {
+        next(error);
+    }
+}
